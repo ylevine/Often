9bc0a2f3eacc8b7037f6a5dcf6de373d0471edf9
Won Song <wys.io@live.com>
2014-02-09 22:46:39 -0500

    Address issue #7

--- public/app/note/directives/searchDrtv.js
+++ public/app/note/directives/searchDrtv.js
@@ -2,16 +2,19 @@ angular.module('often.directives')
 	.directive('search', ['noteSvc',
 		function (noteSvc) {
 			return function (scope, element, attr) {
+				var previousSearch = "";
 				var typingTimer;
 				var typingInterval = 500;
 
 				element.on('keyup', function () {
-					var ctrlScope = scope.$$childTail;
-					ctrlScope.loading = true;
-					ctrlScope.notes = [];
+					if (element[0].value != previousSearch) {
+						previousSearch = element[0].value;
 
-					// Search when typing stopes.
-					if (element[0].value.length > 2) {
+						var ctrlScope = scope.$$childTail;
+						ctrlScope.loading = true;
+						ctrlScope.notes = [];
+
+						// Search when typing stopes.
 						typingTimer = setTimeout(function () {
 							noteSvc.search(element[0].value, function (data) {
 								ctrlScope.notes = data;
@@ -26,11 +29,11 @@ angular.module('often.directives')
 				});
 
 				// Reset the search input box on all route changes
-				scope.$on('$routeChangeSuccess', function(evt, cur, prev) {
+				scope.$on('$routeChangeSuccess', function (evt, cur, prev) {
 					element.val("");
 				});
 
-				scope.$on('searchResetEvent', function() {
+				scope.$on('searchResetEvent', function () {
 					element.val("");
 				})
 			};
